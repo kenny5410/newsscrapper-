d838bf90a91ec29fbba4744a04d1cfe8515702be
Kenny <nels5410@gmail.com>
2019-04-14 17:32:20 -0500

    Updated server/routes

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+var PORT = 3000;
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+})
